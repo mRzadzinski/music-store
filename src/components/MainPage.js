4f38e744699760470c9253f17ab701281d7b5edc
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-05-03 20:50:40 +0200

    Style MainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import '../styles/MainPage.scss';
+import mainImg from '../img/fender-guitars.jpg';
 
 const MainPage = () => {
-	return <div className='MainPage'>MainPage</div>;
+	return (
+		<div className='MainPage'>
+			<div className='main-left'>
+				<div className='adv-title'>ELECTRIC GUITARS</div>
+				<div className='adv-content'>Get yours today!</div>
+				<div className='shop-btn-container'>
+					<button id='shop-now-btn'>SHOP NOW</button>
+				</div>
+			</div>
+			<div className='main-img-container'>
+				<img id='main-img' src={mainImg} alt='fender-guitars' />
+			</div>
+		</div>
+	);
 };
 
 export default MainPage;
